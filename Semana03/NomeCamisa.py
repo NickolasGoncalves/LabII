1247e70aad8533b3410fcd1ecbf2f75a44808926
NickolasGoncalves <126731211+NickolasGoncalves@users.noreply.github.com>
2023-08-31 11:00:59 -0300

    Create NomeCamisa.py

---
+++ Semana03/NomeCamisa.py
@@ -0,0 +1,15 @@
+def main():
+    nome = {
+        'Gustavo' : 'Preta',
+        'Níckolas' : 'Branca',
+        'Guilherme' : 'Verde',
+        'Pedro' : 'Azul',
+        'João' : 'Vermelho',
+    }
+    
+    for k,v in nome.items():
+        print(f"Nome: {k} / / / Cor da Camisa: {v}")
+
+
+
+main()
