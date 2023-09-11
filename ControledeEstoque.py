bd36f21ae2dabfda89c84da29365288f2a0c872b
NickolasGoncalves <126731211+NickolasGoncalves@users.noreply.github.com>
2023-09-11 18:01:43 -0300

    Update ControledeEstoque.py

--- ControledeEstoque.py
+++ ControledeEstoque.py
@@ -1,3 +1,14 @@
+
+def menu():
+    print("## MERCADINHO DO SEU ZÉ ##")
+    print("1- Adicionar um produto: ")
+    print("2- Buscar um produto: ")
+    print("3- Vizualizar todos os produtos: ")
+    print("4- Vender um produto: ")
+    print("5- Relatório de vendas: ")
+    opc = int(input("O que você deseja? "))
+    return opc
+
 def adicionar_produto(my_dict):
     product_name = input("Digite o nome do produto (ou deixe vazio para sair): ")
     
@@ -11,16 +22,22 @@ def adicionar_produto(my_dict):
       "amount": product_amount,
       "price": product_price
     }
-
+    for product_name, product_info in my_dict.items():
+        amount = product_info["amount"]
+        price = product_info["price"]
+        print(f"Produto: {product_name}, Quantidade: {amount}, Preço: {price}")
+        
 def main():
     my_dict= {}
     
     while True:
-        adicionar_produto(my_dict)
+        opc = menu()
+        if opc == 1:
+            while True:
+                adicionar_produto(my_dict)
+                if input("Deseja adicionar outro produto? (s/n): ").lower() != "s":
+                    break
         
-       
-        if input("Deseja adicionar outro produto? (s/n): ").lower() != "s":
-            break
     
   
 
