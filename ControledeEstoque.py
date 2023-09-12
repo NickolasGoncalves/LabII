69bb8b5a43ed3eaf36d754b49500855159cbe925
NickolasGoncalves <126731211+NickolasGoncalves@users.noreply.github.com>
2023-09-12 11:46:06 -0300

    Update ControledeEstoque.py

--- ControledeEstoque.py
+++ ControledeEstoque.py
@@ -26,6 +26,43 @@ def adicionar_produto(my_dict):
         amount = product_info["amount"]
         price = product_info["price"]
         print(f"Produto: {product_name}, Quantidade: {amount}, Preço: {price}")
+
+def buscar_produto(my_dict):
+    busca = input("Digite o nome do Produto: ")
+        
+    if busca in my_dict:
+        print(f"Procurando {busca}...")
+        for product_name, product_info in my_dict.items():
+            amount = product_info["amount"]
+            price = product_info["price"]
+            print(f"Produto: {product_name}, Quantidade: {amount}, Preço: {price}")
+    else:
+        print(f"Produto {busca} não registrado")
+
+def produto_estoque(my_dict):
+    for product_name, product_info in my_dict.items():
+        amount = product_info["amount"]
+        price = product_info["price"]
+        print(f"Produto: {product_name}, Quantidade: {amount}, Preço: {price}")
+
+def venda_produto(my_dict):
+    product_name = input("Digite o nome do produto vendido: ")
+    quantity_sold = int(input("Digite a quantidade vendida: "))
+
+    if product_name in my_dict:
+        product = my_dict[product_name]
+        current_quantity = product['amount']
+        
+        if quantity_sold <= current_quantity:
+            product['amount'] -= quantity_sold
+            sale_value = quantity_sold * product['price']
+            print(f"Venda realizada com sucesso! Valor total: R$ {sale_value}")
+        else:
+            print("Quantidade em estoque insuficiente.")
+    else:
+        print("Produto não encontrado no estoque.")
+
+
         
 def main():
     my_dict= {}
@@ -37,6 +74,16 @@ def main():
                 adicionar_produto(my_dict)
                 if input("Deseja adicionar outro produto? (s/n): ").lower() != "s":
                     break
+        elif opc == 2:
+            buscar_produto(my_dict)
+        elif opc == 3:
+            produto_estoque(my_dict)
+        elif opc == 4:
+            while True:
+                venda_produto(my_dict)
+                if input("Deseja comprar outro produto? (s/n): ").lower() != "s":
+                    break
+
         
     
   
