5002a37a6629f9649bd3c6c3475a8d13ddbcb038
NickolasGoncalves <126731211+NickolasGoncalves@users.noreply.github.com>
2023-09-19 20:35:03 -0300

    Update RevisaoProva_lista.py

--- Revisao/RevisaoProva_lista.py
+++ Revisao/RevisaoProva_lista.py
@@ -1,1 +1,13 @@
+my_list = [8 ,6 ,9 ,4 ,2 ,3 ,5 ,7 , 1]
 
+for index_one in range(len(my_list)):
+  min_index = index_one
+  
+  for index_two in range(index_one, len(my_list)):
+    if my_list[index_two] < my_list[min_index]
+    min_index = index_two
+
+
+min_value = my_list[min_index]
+my_list[min_index] = my_list[index_one]
+my_list[index_one] = min_value
