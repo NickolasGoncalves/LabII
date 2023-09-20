e79350ff7adf8f0bb8c27956a161be7aeb38cdb0
NickolasGoncalves <126731211+NickolasGoncalves@users.noreply.github.com>
2023-09-19 21:51:26 -0300

    Update RevisaoProva_lista.py

--- Revisao/RevisaoProva_lista.py
+++ Revisao/RevisaoProva_lista.py
@@ -12,6 +12,7 @@ min_value = my_list[min_index]
 my_list[min_index] = my_list[index_one]
 my_list[index_one] = min_value
 '''
+'''
 for index in range(len(my_list):              
   for index_one in range(len(my_list)):
     if (index_one + 1) < len(my_list) and my_list[index_one] > my_list[index_one + 1]
@@ -19,6 +20,35 @@ for index in range(len(my_list):
     my_list[index_one] = my_list[index + 1]
   
 print(my_list)
+'''
 
+matriz = [
+    [8 ,6 ,9] ,
+    [4 ,2 ,3] ,
+    [5 ,7 , 1]
+]
+
+
+
+for line in range(len(matriz)):
+    for column in range(len(matriz[line])):
+        min_line = line
+        min_column = column
+
+        for line_aux in range(line, len(matriz)):
+            start = column if line == line_aux else 0
+            
+            for column_aux in range(start, len(matriz[line_aux])):
+                if matriz[line_aux][column_aux] < matriz[min_line][min_column]:
+                    min_line = line_aux
+                    min_column = column_aux
+            
+        min_value = matriz[min_line][min_column]
+        matriz[min_line][min_column] = matriz[line][column]
+        matriz[line][column] = min_value
+
+print(matriz)
+            
+            
 
 
